a57ec6c30cc579cc68a4c0363a3b4b760fbe818a
ejas404 <ijasjaaass004@gmail.com>
2023-11-20 15:07:32 +0530

    dfs graph

--- graph.js
+++ graph.js
@@ -2,27 +2,48 @@ let graphAdjacency = {
     'a':['b','c'],
     'b':['a','d','e'],
     'c':['a','d','f'],
-    'd': ['b','c']
+    'd': ['b','c'],
+    'f':[],
+    'e':[]
 
 }
 
 
-function graphDfs(graphAdjacency){
-   let check = new Set()
-   let res = []
+function dfs(){
+  let check = new Set()
+  let stack = []  
 
-   function dfsSearch(){
-
-   }
-    
-    for(let vrtx in graphAdjacency){
-        for(let each of graphAdjacency[vrtx]){
-            if(check.has(each)) continue;
-            res.push(each)
-            check.add(each)
+  function traverse(node){
+        if(check.has(node)) return;
+        check.add(node)
+        for(let vrtx of graphAdjacency[node]){
+            traverse(vrtx)
         }
-    }
-    return res
+  }
+
+  traverse('e')
+  return [...check]
 }
 
-console.log(graphDfs(graphAdjacency))
+console.log(dfs())
+
+
+// function graphDfs(graphAdjacency){
+//    let check = new Set()
+//    let res = []
+
+//    function dfsSearch(){
+
+//    }
+    
+//     for(let vrtx in graphAdjacency){
+//         for(let each of graphAdjacency[vrtx]){
+//             if(check.has(each)) continue;
+//             res.push(each)
+//             check.add(each)
+//         }
+//     }
+//     return res
+// }
+
+// console.log(graphDfs(graphAdjacency))
