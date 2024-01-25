fb83532effa0eb7eca41a76bc888e074019077db
ejas404 <ijasjaaass004@gmail.com>
2024-01-25 11:29:28 +0530

    find the smallest elem in arr recursively

--- recursion/sum.js
+++ recursion/sum.js
@@ -5,4 +5,20 @@ function sum(n){
     return n+sum(n-1)
 }
 
-console.log(sum(3))
+// console.log(sum(3))
+
+// Write a recursive function that finds and returns the minimum element in an array, where the array and its size are given as parameters.
+
+const arr1 = [5,6,7,8,1]
+const arr1Length =  arr1.length
+let smallest = Infinity
+
+function findSmallest(arr, length,smallest){
+    if(length === 0) return smallest;
+    if(arr[arr1Length - 1] < smallest){
+        smallest = arr1[length-1]
+    }
+    return findSmallest(arr,length-1,smallest)
+}
+
+// console.log(findSmallest(arr1, arr1Length, smallest),'smallest element')
